d2b0f4093799ac3695bd0e89b4ee8a044c68bfb5
Shyam9583 <strikerallin1@gmail.com>
2020-05-27 17:40:17 +0530

    changed database

--- src/main/kotlin/com/pce/kotlin/bookmetutor/configuration/CorsConfiguration.kt
+++ src/main/kotlin/com/pce/kotlin/bookmetutor/configuration/CorsConfiguration.kt
@@ -1,19 +1,23 @@
 package com.pce.kotlin.bookmetutor.configuration
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import java.util.concurrent.Executor
 
-@Bean
-fun getEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
+@Configuration
+class BeanProvider {
+    @Bean
+    fun getEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
 
-@Bean("taskExecutor")
-fun getExecutor(): Executor {
-    return ThreadPoolTaskExecutor().apply {
-        corePoolSize = 2
-        maxPoolSize = 2
-        setQueueCapacity(500)
-        initialize()
+    @Bean("taskExecutor")
+    fun getExecutor(): Executor {
+        return ThreadPoolTaskExecutor().apply {
+            corePoolSize = 2
+            maxPoolSize = 2
+            setQueueCapacity(500)
+            initialize()
+        }
     }
 }
